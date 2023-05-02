f798083b07931e18955cdc28754503ea824be499
Suryansh <suryanshgrover1999@gmail.com>
2023-05-02 15:13:15 +0530

    learnt the difference between static methods and instance methods

--- 06__Arrays/Transpose.java
+++ 06__Arrays/Transpose.java
@@ -2,7 +2,22 @@ import java.util.Arrays;
 
 public class Transpose {
 
-    public static int[][] transpose(int[][] arr) {
+    // public static int[][] transpose(int[][] arr) {
+    // int rows = arr.length;
+    // int columns = arr[0].length;
+
+    // int[][] transposedArr = new int[columns][rows];
+
+    // for (int i = 0; i < rows; i++) {
+    // for (int j = 0; j < columns; j++) {
+    // transposedArr[j][i] = arr[i][j];
+    // }
+    // }
+    // return transposedArr;
+    // }
+
+    // Instance Method
+    public int[][] transpose(int[][] arr) {
         int rows = arr.length;
         int columns = arr[0].length;
 
@@ -18,7 +33,11 @@ public class Transpose {
 
     public static void main(String[] args) {
         int[][] arr = { { 1, 2, 3 }, { 4, 5, 6 } };
-        int[][] tranposedArr = transpose(arr);
+        // int[][] tranposedArr = transpose(arr);
+
+        // When creating methods without making them "static"
+        Transpose transpose_obj = new Transpose();
+        int[][] transposedArr = transpose_obj.transpose(arr);
 
         System.out.println("Orginal Array: ");
         for (int[] row : arr) {
@@ -26,7 +45,7 @@ public class Transpose {
         }
 
         System.out.println("\nTransposed Array: ");
-        for (int[] row : tranposedArr) {
+        for (int[] row : transposedArr) {
             System.out.println(Arrays.toString(row));
         }
     }
